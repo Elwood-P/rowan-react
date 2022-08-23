a87e868a8ac0072565f8ffcf340d8a02e3bdcc93
Elwood-P <info@pjlittlewood.co.uk>
2022-08-23 16:49:48 +0100

    Update App to use useRoutes hook

---
+++ src/components/UI/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Header />
+        
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
