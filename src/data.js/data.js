1bbbabef2d51f26b1547028a496384d67393de04
Elwood-P <info@pjlittlewood.co.uk>
2022-08-23 11:15:49 +0100

    Inital commit

---
+++ src/data.js/data.js
@@ -0,0 +1,43 @@
+const DUMMY_PRODUCTS = [
+  {
+    id: '2cf067ef-a37b-435a-92b6-9ad058631e5c',
+    name: 'Aztec Autumn Wool Jumper',
+    description:
+      'Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas faucibus mollis interdum.',
+    imageurl: 'https://kimba.imgix.net/wp-content/uploads/2021/08/jumper-zigzag.jpg',
+    category: 'knitwear',
+    material: 'ethically sourced wool',
+    price: 90,
+    onsale: 'true',
+    saleprice: 70,
+    sizes: ['sm', 'md', 'lg', 'xl', '2xl'],
+  },
+  {
+    id: 'f8cf679e-fc44-44c1-8afe-37d2ac56f836',
+    name: 'Felted Wool Jumper',
+    description:
+      'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor.',
+    imageurl: 'https://kimba.imgix.net/wp-content/uploads/2021/09/shirt-stars.jpg',
+    category: 'knitwear',
+    material: 'ethically sourced wool',
+    price: 70,
+    onsale: 'false',
+    saleprice: null,
+    sizes: ['sm', 'md', 'lg', 'xl', '2xl'],
+  },
+  {
+    id: 'ec26c6ad-926a-497a-b631-32ee073c7b15',
+    name: 'Forest Green Sherpa Fleece',
+    description:
+      'Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue.',
+    imageurl: 'https://kimba.imgix.net/wp-content/uploads/2021/09/jumper-forest.jpg',
+    category: 'knitwear',
+    material: 'ethically sourced wool',
+    price: 110,
+    onsale: 'false',
+    saleprice: null,
+    sizes: ['sm', 'md', 'lg', 'xl', '2xl'],
+  },
+];
+
+export default DUMMY_PRODUCTS;
