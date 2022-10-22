de9b466952b7e6e7353a57139094dd1a592d313d
Elwood-P <info@pjlittlewood.co.uk>
2022-10-22 17:01:38 +0100

    Fix shop  so sort and filter work together

--- src/components/Pages/ShopPage.jsx
+++ src/components/Pages/ShopPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
+import { cloneDeep } from 'lodash';
 
 import FilterMenu from '@/components/Shop/FilterMenu/FilterMenu';
 import SortDropdown from '@/components/Shop/SortDropdown/SortDropdown';
@@ -9,25 +10,43 @@ import ProductList from '@/components/Shop/ProductsList/ProductList';
 const ShopPage = () => {
   const allProducts = useSelector((state) => state.products);
   const [filteredProducts, setFilteredProducts] = useState(allProducts);
-  const [sortedFilteredProducts, setSortedFilteredProducts] = useState(allProducts);
+  const [sortOption, setSortOption] = useState('default');
 
   const updateFilteredProducts = (updatedFilteredProducts) => {
     setFilteredProducts(updatedFilteredProducts);
-    updateSortedProducts(updatedFilteredProducts);
   };
 
-  const updateSortedProducts = (updatedSortedProducts) => {
-    setSortedFilteredProducts(updatedSortedProducts);
+  const updateSortOption = (updatedSortOption) => {
+    setSortOption(updatedSortOption);
   };
+
+  // NB Sorting function can't go in sort dropdown component since it also needs to be called after FilterMenu udpate
+  // TODO: theres probarbly a better way to organise this? - move filter and sort logic into custom hooks
+  const sortedProducts = cloneDeep(filteredProducts);
+  const sortProducts = () => {
+    console.log(sortOption);
+    if (sortOption !== 'default') {
+      sortedProducts.sort((a, b) => {
+        switch (sortOption) {
+          case 'priceAsc':
+            return a.price <= b.price ? -1 : 1;
+          case 'priceDesc':
+            return a.price >= b.price ? -1 : 1;
+        }
+      });
+    }
+  };
+
+  sortProducts();
 
   return (
     <div className="container-fluid | flex flex-col gap-8">
       <header className="flex flex-col items-center space-y-6 | my-fl-400">
         <h1 className="font-serif font-normal text-800">Shop</h1>
       </header>
-      <SortDropdown allProducts={allProducts} filteredProducts={filteredProducts} onSortChange={updateSortedProducts} />
+      <SortDropdown allProducts={allProducts} sortOption={sortOption} onSortChange={updateSortOption} />
       <FilterMenu allProducts={allProducts} filteredProducts={filteredProducts} onFilterChange={updateFilteredProducts} />
-      <ProductList products={sortedFilteredProducts} />
+      <ProductList products={sortedProducts} />
     </div>
   );
 };
