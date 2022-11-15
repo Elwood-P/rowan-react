62716e3ed9808e9cd72488f1e2efd626cd61f712
Elwood-P <info@pjlittlewood.co.uk>
2022-11-15 14:08:27 +0000

    ScrollToTop Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import Header from '@/components/Header/Header';
 import MiniCart from '@/components/Shop/Cart/MiniCart';
 import Footer from '@/components/Footer/Footer';
-import Notifications from '@/components/UI/Notifications';
 import ShopPage from '@/components/Pages/ShopPage';
 import BasketPage from '@/components/Pages/BasketPage';
 import ProductPage from '@/components/Pages/ProductPage';
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '@/store';
+import ScrollToTop from '@/components/UI/ScrollToTop';
 import App from '@/App';
 import '@/styles/app.scss';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
