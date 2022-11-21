e4f700709f67017684d07c812633714d4e1fae06
Elwood-P <info@pjlittlewood.co.uk>
2022-11-21 19:08:42 +0000

    Update HeaderNav Styles

--- src/components/Header/HeaderNav/HeaderNav.jsx
+++ src/components/Header/HeaderNav/HeaderNav.jsx
@@ -7,11 +7,11 @@ const HeaderNav = () => {
   return (
     <nav className="relative">
       <div
-        className="absolute | w-full | py-14 | border-t border-b border-black border-opacity-25 | bg-cream bg-opacity-95 | z-40"
+        className="absolute | w-full | py-14 | border-t border-b border-black border-opacity-25 | bg-cream bg-opacity-90 | z-40"
         style={{ backdropFilter: 'blur(3px)' }}
       >
         <div className="container-fluid">
-          <menu id="menu-primary-navigation" className="flex justify-between">
+          <menu id="menu-primary-navigation" className="flex gap-x-24">
             {headerNavMenus.map((menu) => (
               <HeaderNavMenu key={menu.title} menu={menu} />
             ))}
