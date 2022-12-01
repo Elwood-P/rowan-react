650859f51e9c1bf85579845db5d4ab6e1eda621b
Elwood-P <info@pjlittlewood.co.uk>
2022-12-01 12:58:35 +0000

    Add remove/close button

--- src/components/UI/Notification.jsx
+++ src/components/UI/Notification.jsx
@@ -1,10 +1,11 @@
 import toast, { Toaster, resolveValue } from 'react-hot-toast';
 import { ReactComponent as ExclamationIcon } from '@/assets/images/icons/exclamation-icon.svg';
+import { ReactComponent as CloseIcon } from '@/assets/images/icons/close-icon.svg';
 
 const Notification = () => {
   const toastOptions = {
     position: 'top-right',
-    duration: 4000,
+    duration: 3000,
     style: { animation: '' }, // TODO: Animation
   };
 
@@ -12,11 +13,14 @@ const Notification = () => {
     <Toaster toastOptions={toastOptions}>
       {(t) => (
         <div
-          className="flex gap-4 items-center | w-[320px] | p-4 | bg-black rounded-md | text-cream font-normal | drop-shadow-tiny | cursor-pointer"
+          className="flex gap-4 items-center | w-[320px] | p-4 pr-6 | bg-black rounded-md | text-300 text-cream font-normal | drop-shadow-tiny"
           onClick={() => toast.remove(t.id)}
         >
           {t.type === 'error' && <ExclamationIcon className="w-[20px] min-w-[20px]" />}
           {resolveValue(t.message, t)}
+          <button onClick={() => toast.remove(t.id)} className="absolute top-3 right-3">
+            <CloseIcon className="w-[12px]"/>
+          </button>
         </div>
       )}
     </Toaster>
