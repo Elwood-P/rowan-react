9c8485a48da53f857d828da2227c293f4942c091
Elwood-P <info@pjlittlewood.co.uk>
2022-12-31 14:16:11 +0000

    404 Page Styling

--- src/components/Pages/PageNotFound.jsx
+++ src/components/Pages/PageNotFound.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div>
-      404: Page Not Found
+    <div className="container-fluid | py-16 my-fl-400 | text-center">
+      <h1 className="font-serif font-normal text-800 | mb-2">404</h1>
+      <p className="mb-2 | text-600 font-semibold uppercase tracking-wide">Page Not Found</p>
+      <Link className="underline" to="/">Shop Front</Link>
     </div>
   );
 };
